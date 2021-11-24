03a5a9e11e1ea50926574105e31957446f8a171d
Yurii Smolii <yurasmoliy.y@gmail.com>
2021-11-24 23:43:53 +0200

    add balancer & asp

---
+++ dotnetapp/Controllers/HomeController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+using dotnetapp.Models;
+
+namespace dotnetapp.Controllers;
+public class HomeController : Controller
+{
+    public IActionResult Index()
+    {
+        ViewData["Machine"] = Environment.MachineName;
+        
+        return View();
+    }
+
+    [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+    public IActionResult Error()
+    {
+        return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+    }
+}
---
+++ dotnetapp/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using Hangfire;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Hangfire.MemoryStorage;
+
+var builder = WebApplication.CreateBuilder(args);
+
+
+// Add services to the container.
+builder.Services.AddControllersWithViews();
+builder.Services.AddHangfire(config =>
+    config.SetDataCompatibilityLevel(CompatibilityLevel.Version_170)
+        .UseSimpleAssemblyNameTypeSerializer()
+        .UseDefaultTypeSerializer()
+        .UseMemoryStorage());
+builder.Services.AddHangfireServer();
+builder.Services.AddHealthChecks();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Home/Error");
+}
+
+app.UseStaticFiles();
+app.UseRouting();
+
+app.MapHealthChecks("/health");
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=Home}/{action=Index}/{id?}");
+app.UseHangfireDashboard();
+
+app.Run();
